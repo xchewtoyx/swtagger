373cb981c5fab9637b7bbd8d949114f4864eb6c4
Russell Heilling <chewtoy@s8n.net>
2012-03-17 01:41:31 +0000

    Packaging updates

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ setup(
     version = "0.1",
     packages=["shotwelldb"],
     scripts=["bin/swtagger.py"],
+    author='Russell Heilling',
+    author_email='chewtoy@s8n.net',
+    url='http://russell.heilling.net/scripts',
     license="MIT",
     long_description=open('README.txt').read())
